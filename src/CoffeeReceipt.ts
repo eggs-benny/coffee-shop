d91799714a4f5ecc7c77c7cced697b93bde551d9
eggs-benny <benedict.smith87@gmail.com>
2023-02-02 17:56:03 +0000

    test pass shop name from JSON

--- src/CoffeeReceipt.ts
+++ src/CoffeeReceipt.ts
@@ -1,5 +1,23 @@
+import shopData from './exampleCoffeeInfo.json';
+
 export default class Receipt {
   getShopName(): string {
-    return 'The Coffee Connection';
+    const data: Array<any> = this.getJsonData()
+
+    return data[0].shopName
+  }
+
+  getJsonData(): object[] {
+    return shopData;
   }
 }
+
+
+// [
+//   {
+//     shopName: 'The Coffee Connection',
+//     address: '123 Lakeside Way',
+//     phone: '16503600708',
+//     prices: [ [Object] ]
+//   }
+// ]
--- src/spec/CoffeeReceipt.spec.ts
+++ src/spec/CoffeeReceipt.spec.ts
@@ -1,4 +1,32 @@
 import Receipt from './../CoffeeReceipt';
+//import ShopData from './exampleCoffeeInfo.json';
+
+const shopData = [
+  {
+    shopName: 'The Coffee Connection',
+    address: '123 Lakeside Way',
+    phone: '16503600708',
+    prices: [
+      {
+        'Cafe Latte': 4.75,
+        'Flat White': 4.75,
+        "Cappucino": 3.85,
+        'Single Espresso': 2.05,
+        'Double Espresso': 3.75,
+        "Americano": 3.75,
+        "Cortado": 4.55,
+        "Tea": 3.65,
+        'Choc Mudcake': 6.4,
+        'Choc Mousse': 8.2,
+        "Affogato": 14.8,
+        "Tiramisu": 11.4,
+        'Blueberry Muffin': 4.05,
+        'Chocolate Chip Muffin': 4.05,
+        'Muffin Of The Day': 4.55
+      }
+    ]
+  }
+];
 
 describe('Reciept', () => {
   test('should return the shop name from the json file', () => {
@@ -6,4 +34,11 @@ describe('Reciept', () => {
 
     expect(receipt.getShopName()).toEqual('The Coffee Connection');
   });
+
+  test('should return data object from json file', () => {
+    const receipt = new Receipt();
+
+    expect(receipt.getJsonData()).toEqual(shopData);
+  });
 });
+
