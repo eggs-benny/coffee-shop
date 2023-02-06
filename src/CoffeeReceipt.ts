b804947db5a7b10774f8ca1bb954c3d2fcf923c1
eggs-benny <benedict.smith87@gmail.com>
2023-02-06 17:16:17 +0000

    tests pass: header format & item price call

--- src/CoffeeReceipt.ts
+++ src/CoffeeReceipt.ts
@@ -1,23 +1,41 @@
-import shopData from './exampleCoffeeInfo.json';
+//import shopData from './exampleCoffeeInfo.json';
 
 export default class Receipt {
+  getJsonData(): object[] {
+    const shopData = require('./exampleCoffeeInfo.json')
+
+    return shopData
+  }
+
   getShopName(): string {
     const data: Array<any> = this.getJsonData()
-
     return data[0].shopName
   }
 
-  getJsonData(): object[] {
-    return shopData;
+  getAddress(): string {
+    const data: Array<any> = this.getJsonData()
+    return data[0].address
+  }
+
+  getPhone(): string {
+    const data: Array<any> = this.getJsonData()
+    return data[0].phone
+  }
+
+  getHeader(): string {
+    const shopName: string = this.getShopName()
+    const address: string = this.getAddress()
+    const phone: string = this.getPhone()
+  
+    return `${shopName}\n\n${address}\nPhone: +${phone[0]} (${phone.slice(1,4)}) ${phone.slice(4,7)}-${phone.slice(7,11)}`
+  }
+
+  getItemPrice(itemName: string): string {
+    const data: Array<any> = this.getJsonData()
+    const prices = data[0].prices[0]
+    const price = prices[`${itemName}`]
+  
+    return price
+    
   }
 }
-
-
-// [
-//   {
-//     shopName: 'The Coffee Connection',
-//     address: '123 Lakeside Way',
-//     phone: '16503600708',
-//     prices: [ [Object] ]
-//   }
-// ]
--- src/spec/CoffeeReceipt.spec.ts
+++ src/spec/CoffeeReceipt.spec.ts
@@ -1,7 +1,7 @@
 import Receipt from './../CoffeeReceipt';
 //import ShopData from './exampleCoffeeInfo.json';
 
-const shopData = [
+const expectedShopData = [
   {
     shopName: 'The Coffee Connection',
     address: '123 Lakeside Way',
@@ -10,16 +10,16 @@ const shopData = [
       {
         'Cafe Latte': 4.75,
         'Flat White': 4.75,
-        "Cappucino": 3.85,
+        'Cappucino': 3.85,
         'Single Espresso': 2.05,
         'Double Espresso': 3.75,
-        "Americano": 3.75,
-        "Cortado": 4.55,
-        "Tea": 3.65,
+        'Americano': 3.75,
+        'Cortado': 4.55,
+        'Tea': 3.65,
         'Choc Mudcake': 6.4,
         'Choc Mousse': 8.2,
-        "Affogato": 14.8,
-        "Tiramisu": 11.4,
+        'Affogato': 14.8,
+        'Tiramisu': 11.4,
         'Blueberry Muffin': 4.05,
         'Chocolate Chip Muffin': 4.05,
         'Muffin Of The Day': 4.55
@@ -27,6 +27,37 @@ const shopData = [
     ]
   }
 ];
+
+// jest.mock(
+//   '../exampleCoffeeInfo.json',
+//   () => [
+//     {
+//       shopName: 'The Coffee Connection',
+//       address: '123 Lakeside Way',
+//       phone: '16503600708',
+//       prices: [
+//         {
+//           'Cafe Latte': 4.75,
+//           'Flat White': 4.75,
+//           'Cappucino': 3.85,
+//           'Single Espresso': 2.05,
+//           'Double Espresso': 3.75,
+//           'Americano': 3.75,
+//           'Cortado': 4.55,
+//           'Tea': 3.65,
+//           'Choc Mudcake': 6.4,
+//           'Choc Mousse': 8.2,
+//           'Affogato': 14.8,
+//           'Tiramisu': 11.4,
+//           'Blueberry Muffin': 4.05,
+//           'Chocolate Chip Muffin': 4.05,
+//           'Muffin Of The Day': 4.55
+//         }
+//       ]
+//     }
+//   ],
+//   { virtual: true }
+// );
 
 describe('Reciept', () => {
   test('should return the shop name from the json file', () => {
@@ -38,7 +69,26 @@ describe('Reciept', () => {
   test('should return data object from json file', () => {
     const receipt = new Receipt();
 
-    expect(receipt.getJsonData()).toEqual(shopData);
+    expect(receipt.getJsonData()).toEqual(expectedShopData);
   });
-});
 
+  test('should return the address & phone from the json file', () => {
+    const receipt = new Receipt();
+
+    expect(receipt.getAddress()).toEqual('123 Lakeside Way');
+    expect(receipt.getPhone()).toEqual('16503600708');
+  });
+
+  test('should return a formatted header', () => {
+    const receipt = new Receipt();
+
+    expect(receipt.getHeader()).toEqual('The Coffee Connection\n\n123 Lakeside Way\nPhone: +1 (650) 360-0708');
+  });
+
+  test('should return an item`s price', () => {
+    const receipt = new Receipt();
+
+    expect(receipt.getItemPrice('Cafe Latte')).toEqual(4.75);
+  });
+
+});
